6015255549df2b9a9ddbb0a145ea2786945e7c4b
Karinne App <k.app@students.chapelfield.org>
2019-05-26 23:17:42 -0400

    Add code that tells the program what to do using if statements

--- GuessTheNumber-bear-PC.py
+++ GuessTheNumber-bear-PC.py
@@ -1,4 +1,4 @@
-#Guess the Number Game
+#G#Guess the Number Game
 import random
 
 guessesTaken = 0
@@ -15,6 +15,24 @@ while guessesTaken < 6:
       guess = int(guess)
 
       gussesTaken = gussesTaken + 1
+
+      if guess < number:
+            print('Your guess is too low.')
+
+      if guess > number:
+            print('Your guess is too high.')
+
+      if guess == number:
+             break
+
+if guess == number:
+      guessesTaken =str(guessesTaken)
+      print('Good jod, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses! ')
+
+if guess != number:
+      number = str(number)
+      print('Awwe you got it wrong. The number I was thinking of was ' + number + ' Try agian next time! ')
+      
       
       
 
