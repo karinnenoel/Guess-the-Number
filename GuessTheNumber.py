fe52d1e84c38e78d7ab6c8aa9e2f7aa387acab25
Karinne App <k.app@students.chapelfield.org>
2019-05-26 21:34:30 -0400

    Add code that will print when program is being run

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,2 +1,13 @@
 #Guess the Number Game
+import random
+
+guessesTaken = 0
+
+print('Hello! Welcome to The Guess the Number Game. What is your name?')
+myName = input()
+
+number = random.randint(1, 100)
+print ('Well, ' + myName + ', I am thinking of a number between 1 and 100.')
+
+
 
