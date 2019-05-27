1a627ec4402fbe09290c221081e70390cf79e233
Karinne App <k.app@students.chapelfield.org>
2019-05-26 20:58:36 -0400

    Conclude merge

---
+++ GuessTheNumber-bear-PC.py
@@ -0,0 +1,28 @@
+#Guess the Number Game
+
+<<<<<<< HEAD
+=======
+import random
+
+guesses = 6
+number = random.randint(1, 100)
+win = False
+
+while guesses > 0;
+      guess = int(input("Guess: "))
+
+      guesses -= 1
+      
+       if guess > number:
+          print("Your guess was too high, you have", guesses, "remaining")
+      elif guess < number:
+         print ("Your guess was too low, you have", guesses, "remaining")
+     else:
+         print ("YAY! You Won!! Your guess was correct.")
+         win = True
+         guesses = 0
+
+
+if win == False:
+    print("sorry, you didn't guess the number. :( The number was", number)
+>>>>>>> 92b5bdb98c27d5bbc80c285882eb2edc8bee4754
