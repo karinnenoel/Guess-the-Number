ab0d1aa7b77da82ac8c10233f60ecacb6aa546ea
Karinne App <k.app@students.chapelfield.org>
2019-05-26 22:48:55 -0400

    Add code that will manage how many guesses a user has

--- GuessTheNumber-bear-PC.py
+++ GuessTheNumber-bear-PC.py
@@ -9,4 +9,12 @@ myName = input()
 number = random.randint(1, 100)
 print ('Well, ' + myName + ', I am thinking of a number between 1 and 100.')
 
+while guessesTaken < 6:
+      print('Take a guess.')
+      guess = input()
+      guess = int(guess)
+
+      gussesTaken = gussesTaken + 1
+      
+      
 
