5250ca6d97e24a71df2ef2b01efabfdb1e530e9b
Karinne App <k.app@students.chapelfield.org>
2019-05-30 15:17:45 -0400

    In the process of fixing ints and strs

--- GuessTheNumber-bear-PC.py
+++ GuessTheNumber-bear-PC.py
@@ -6,34 +6,24 @@ guessesTaken = 0
 print('Hello! Welcome to The Guess the Number Game. What is your name?')
 myName = input()
 
-print(+ myName +', please choose the maximum number in your range.')
-maxNumber = input()
+print(myName +', please choose the maximum number in your range.')
+maxNumber = int(input())
 
-print(+ myName +', please choose the minimum number in your range.') 
-minNumber = input()
-
+print(myName +', please choose the minimum number in your range.') 
+minNumber = int(input())
 
 number = random.randint(minNumber, maxNumber)
-print ('Well, ' + myName + ', I am thinking of a number between' + minNumber + 'and' + maxNumber +.)
-
-guessesTaken == 0
+print ('Well, ' + myName + ', I am thinking of a number between' + minNumber + 'and' + maxNumber +'.')
 
 while guessesTaken < 6:
       print('Take a guess.')
       guess = input()
-      guess = int(guess)       
-      
-      guessesTaken = guessesTaken + 1
+      guess = int(guess)
 
-<<<<<<< HEAD
       guessesTaken = guessesTaken + 1
 
       if guessesTaken == 6:
            break
-=======
-      if guessesTaken == 6:
-             break
->>>>>>> 7093d4e8fadfa683a023498f54880e4772182405
 
       if guess < number:
             print('Your guess is too low.')
@@ -44,18 +34,13 @@ while guessesTaken < 6:
       if guess == number:
              break
 
-      
-if guess != number:
-      number = str(number)
-      print('Awwe you got it wrong. The number I was thinking of was ' + number + ' Try agian next time! ')
-
-
 if guess == number:
       guessesTaken =str(guessesTaken)
       print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses! ')
 
-
-    
+if guess != number:
+      number = str(number)
+      print('Awwe you got it wrong. The number I was thinking of was ' + number + ' Try agian next time! ')
       
       
       
